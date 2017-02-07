54e2be950fb49072b3b4d270a0c1eccbb7b47fd6
flyzebra <852184385@qq.com>
2017-02-07 11:33:48 +0800

    edit

--- Apps/Home/Controller/RunluaController.class.php
+++ Apps/Home/Controller/RunluaController.class.php
@@ -1,9 +1,21 @@
 <?php
+
 namespace Home\Controller;
+
 use Think\Controller;
+
 class RunluaController extends AuthController {
     public function index(){
 //     	echo __ACTION__;
-		$this->display("Runlua/index");
+    	$file_path = "./data/flygo.lua";
+    	if(file_exists($file_path)){
+    		$str = file_get_contents($file_path);//将整个文件内容读入到一个字符串中
+//     		$str = str_replace("\r\n","<br />",$str);
+//     		$str = str_replace("\n","<br />",$str);
+    	}else{
+    		$str = "file not exits!".$file_path;
+    	}
+    	$this->content=$str;
+    	$this->display("Runlua/index");
     }
 }
